10d8542d32d9820bd1ea736517051bba4f8d0238
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2022-10-17 11:45:03 +0800

    fonts WORKS

--- eleSelect.js
+++ eleSelect.js
@@ -1,16 +1,21 @@
 const options = {
-  shapes: [{name:'Rectangle'},{name:'Circle'},{name:'Line'},{name:'Triangle'}],
+  shapes: [
+    { name: "Rectangle" },
+    { name: "Circle" },
+    { name: "Line" },
+    { name: "Triangle" }
+  ],
   images: [],
   fonts: [
-    {name:"Time New Roman", type:"serif"},
-    {name:"Rockwell Nova", type:"serif"},
-    {name:"Minion Pro", type:"serif"},
-    {name:"Lucida", type:"serif"},
-    {name:"Courier", type:"serif"},
-    {name:"Helvetica", type:"san-serif"},
-    {name:"Proxima Nova", type:"san-serif"},
-    {name:"Futura Std", type:"san-serif"},
-    {name:"Public Sans", type:"san-serif"},
-    {name:"Codec Cold Trial", type:"san-serif"}
+    { name: "Times New Roman", type: "serif" },
+    { name: "Rockwell", type: "serif" },
+    { name: "Minion", type: "serif" },
+    { name: "Lucida", type: "serif" },
+    { name: "Courier", type: "serif" },
+    { name: "Helvetica", type: "san-serif" },
+    { name: "Proxima Nova", type: "san-serif" },
+    { name: "Futura", type: "san-serif" },
+    { name: "Public Sans", type: "san-serif" },
+    { name: "Codec", type: "san-serif" }
   ]
 };
